4d81ccb4353ceb36f9f7e21cae1d8b1c2ebcc5d3
Kurchitska Mariia <133865515+MariaKurchitska@users.noreply.github.com>
2023-12-12 15:32:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,156 @@
+const translations = {
+    ua: {
+        name: "Марія Курчицька",
+        summary: "Вітаю! Після короткого, але насиченого досвіду в управлінні продуктом та взаємодії з клієнтами я прийняла рішення поринути в світ веб-розробки в ролі Frontend розробниці. Мої навички в оптимізації процесів та спілкуванні з клієнтами стануть міцним фундаментом для розвитку в новій сфері. Готова до нових викликів, відкрита для навчання і з нетерпінням чекаю можливості внести свій внесок у проекти в області веб-розробки.",
+        city: "Київ, Україна",
+        soft: {
+        soft1: "Управління командою",
+        soft2: "Ефективна комунікація",
+        soft3: "Висока відповідальність та самодисципліна",
+        soft4: "Здатність брати ініціативу",
+        soft5: "Гнучкість",
+        },
+        dates: {
+            date1: "Листопад 2023 - Дотепер",
+            date2: "Червень 2023 - Вересень 2023",
+            date3: "Листопад 2020 - Январь 2021",
+            date4: "Грудень 2022 - Квітень 2023",
+            date5: "Лютий 2020 - Листопад 2022",
+            date6: "Вересень 2019 - Лютий 2020",
+        },
+        respons: "Додаткові обов'язки:",
+        descr: {
+            descr1: "Впровадила систему, яка забезпечила безперебійне виробництво навіть при відключенні електроенергії та усунула простої робочої сили;",
+            descr2: "Перерозподілила ресурси та оптимізувала трудові процеси, що знизило кадрові витрати;",
+            descr3: "Розробила і успішно впровадила систему KPI для кожного відділу, стимулюючи співробітників до досягнення високих планів продажів і виробництва;",
+            descr4: "Переглянула контракти з поточними партнерами, змінивши умови співпраці, що призвело до збільшення обсягів продажів та поліпшення взаєморозуміння на рік вперед.",
+            descr5: "Планування бюджету компанії на 2023 рік;",
+            descr6: "Розрахунки обсягів виробництва та продажів на 2023 рік;",
+            descr7: "Навчання та інтеграція нових/текучих співробітників;",
+            descr8: "Розробка нового продукту для компанії.",
+            descr9: "Успішно встановила міцні відносини з ключовими клієнтами, активно спілкуючись і оперативно реагуючи на виникаючі питання;",
+            descr10: "Впровадила карту торгових точок для оптимізації роботи мерчендайзера, що підвищило привабливість продукції на полиці і призвело до збільшення обсягів продажів;",
+            descr11: "Організовувала та проводила маркетингові заходи, спрямовані на привертання нових клієнтів і збір відгуків про продукцію.",
+            descr12: "Управління запасами компанії;",
+            descr13: "Формування звітів про стан відділу продажів;",
+            descr14: "Управління бюджетами відділів продажів, закупівель і виробництва;",
+            descr15: "Контроль і постановка завдань для виробничого відділу компанії.",
+            descr16: "Здійснювала обробку замовлень для забезпечення своєчасної поставки продукції;",
+            descr17: "Стежила за наявністю товарів і сировини, запобігаючи можливим простоям в постачаннях і виробництві;",
+            descr18: "Активно брала участь у маркетингових заходах, спрямованих на привертання нових клієнтів;",
+            descr19: "Здійснювала контроль за своєчасною звітністю відділів на кінець кожного місяця і забезпечувала швидкий переказ витрат відділів в фінансовий відділ."
+        }, 
+        
+
+    },
+    en:{
+        name: "Maria Kurchitska",
+        summary: "Hi there! After a short but rich experience in product management and customer interaction, I have decided to immerse myself in the world of web development as a Frontend Developer. My skills in process optimization and communication with clients will serve as a solid foundation for development in this new field. I am ready for challenges, open to learning, and eagerly await the opportunity to contribute to projects in the web development area.",
+        city: "Kyiv, Ukraine",
+        soft: {
+        soft1: "Team Management",
+        soft2: "Effective communication",
+        soft3: "Strong responsibility and commitment",
+        soft4: "The ability to take initiative",
+        soft5: "Flexibility",
+        },
+        dates: {
+            date1: "November 2023 - Present",
+            date2: "June 2023 - September 2023",
+            date3: "November 2020 - January 2021",
+            date4: "December 2022 - April 2023",
+            date5: "February 2020 - November 2022",
+            date6: "September 2019 - February 2020",
+        },
+        respons: "Additional Responsibilities:",  
+        descr: {
+            descr1: "Implemented a system that ensured uninterrupted production even in the event of a power outage and eliminated downtime of the workforce;",
+            descr2: "Redistributed resources and optimized work processes, resulting in reduced personnel expenses;",
+            descr3: "Developed and successfully implemented a system of KPI for each department, motivating employees to achieve high sales and production targets;",
+            descr4: "Reviewed contracts with current partners, modifying collaboration terms, leading to increased sales volumes and improved understanding for the next year.",
+            descr5: "Budget planning for the company in 2023;",
+            descr6: "Calculations of production and sales volumes for 2023;",
+            descr7: "Training and integration of new/current employees;",
+            descr8: "Development of a new product for the company.",
+            descr9: "Successfully established strong relationships with key clients, actively engaging and promptly responding to emerging questions;",
+            descr10: "Developed and successfully implemented a system of retail outlets to optimize the merchandiser's work, resulting in increased product attractiveness on the shelf and a boost in sales volumes;",
+            descr11: "Organized and conducted marketing events aimed at attracting new customers and collecting feedback on products.",
+            descr12: "Inventory management for the company;",
+            descr13: "Compilation of reports on the sales department's status;",
+            descr14: "Management of budgets for the sales, procurement and production departments;",
+            descr15: "Oversight and task assignment for the company's production department.",
+            descr16: "Processed orders to ensure timely product delivery.",
+            descr17: "Monitored inventory levels of goods and raw materials, preventing potential delays in supply and production;",
+            descr18: "Actively participated in marketing events aimed at attracting new customers;",
+            descr19: "Controlled timely reporting of departments at the end of each month and ensured the prompt transfer of department expenses to the finance department."
+        }, 
+    },
+    ru: {
+        name: "Мария Курчицкая",
+        summary: "Привет! После краткосрочного, но насыщенного опыта в продуктовом управлении и взаимодействии с клиентами, я приняла решение окунуться в мир веб-разработки в роли Frontend-разработчика. Мои навыки в оптимизации процессов и общении с клиентами станут прочным фундаментом для развития в новой сфере. Готова к новым вызовам, открыта для обучения и с нетерпением жду возможности внести свой вклад в проекты в области веб-разработки.",
+        city: "Киев, Украина",
+        soft: {
+            soft1: "Управление командой",
+            soft2: "Эффективная коммуникация",
+            soft3: "Высокая ответственность и самодисциплина",
+            soft4: "Способность брать на себя инициативу",
+            soft5: "Гибкость",
+        },
+        dates: {
+            date1: "Ноябрь 2023 - Наст. время",
+            date2: "Июнь 2023 - Сентябрь 2023",
+            date3: "Ноябрь 2020 - Январь 2021",
+            date4: "Декабрь 2022 - Апрель 2023",
+            date5: "Февраль 2020 - Ноябрь 2022",
+            date6: "Сентябрь 2019 - Февраль 2020",
+        },
+        respons: "Дополнительные обязанности:",  
+        descr: {
+            descr1: "Внедрила систему, которая которая обеспечила бесперебойное производство даже при отключении электроэнергии и устранила простои рабочей силы;",
+            descr2: "Перераспределила ресурсы и оптимизировала трудовые процессы, что снизило кадровые расходы;",
+            descr3: "Разработала и успешно внедрила систему KPI для каждого отдела, стимулируя сотрудников к достижению высоких планов продаж и производства;",
+            descr4: "Пересмотрела контракты с текущими партнерами, изменив условия сотрудничества, что привело к увеличению объемов продаж и улучшению взаимопонимания на год вперед. ",
+            descr5: "Планирование бюджета компании на 2023 год;",
+            descr6: "Расчеты объемов производства и продаж на 2023 год; ",
+            descr7: "Обучение и внедрение новых/текущих сотрудников;",
+            descr8: "Разработка нового продукта для компании.",
+            descr9: "Успешно установила крепкие отношения с ключевыми клиентами, активно взаимодействуя и оперативно реагируя на возникающие вопросы;",
+            descr10: "Внедрила карту торговых точек для оптимизации работы мерчендайзера, что повысило привлекательность продукции на полке и привело к увеличению объемов продаж;",
+            descr11: "Организовывала и проводила  маркетинговые мероприятия, нацеленные на привлечение новых клиентов и сбор отзывов о продуктах.",
+            descr12: "Управление запасами компании;",
+            descr13: "Формирование отчетов о состоянии отдела продаж;",
+            descr14: "Управление бюджетами отделов продаж, закупок и производства; ",
+            descr15: "Контроль и постановка задач для производственного отдела компании.",
+            descr16: "Осуществляла обработку заказов для обеспечения своевременной поставки продукции;",
+            descr17: "Следила за наличием товаров и сырья, предотвращая возможные простои в поставках и производстве;",
+            descr18: "Активно участвовала в маркетинговых мероприятиях, направленных на привлечение новых клиентов;",
+            descr19: "Осуществляла контроль за своевременной отчетностью отделов по итогам месяца и обеспечивала своевременную передачу расходов отделов в финансовый отдел."
+        },   
+    }
+};
+
+function changeLanguage(lang) {
+    document.querySelector(".header__name").innerText = translations[lang].name;
+    document.querySelector(".info__summary").innerText = translations[lang].summary;
+    document.querySelector(".info__city").innerText = translations[lang].city;
+
+    for (let i = 1; i <= 5; i++) {
+        document.getElementById(`soft${i}`).innerText = translations[lang].soft[`soft${i}`];
+    };
+
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`date${i}`).innerText = translations[lang].dates[`date${i}`];
+    };
+    for (let i = 1; i <= 19; i++) {
+        document.getElementById(`descr${i}`).innerText = translations[lang].descr[`descr${i}`];
+    };
+
+document.querySelectorAll(".respons").forEach(function(element) {
+    element.innerText = translations[lang].respons;
+});
+
+};
+
+function printPage() {
+    window.print();
+}
